104a3aa353071f4e06bcac4db36bec508e341cfd
Feri Czenk <feri.czenk@hotmail.com>
2020-05-28 12:05:21 +0300

    CheckCollision, IsCollision, Distance, Turn

--- BigBallGame/GameEngine.cs
+++ BigBallGame/GameEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,16 @@ namespace BigBallGame
     class GameEngine
     {
         private static bool finished = false;
-        List<Ball> AllBalls = new List<Ball>();
+        private static List<Ball> AllBalls = new List<Ball>();
 
-        public void InitializeGame(PictureBox pb, int regular, int monster, int repelent)
+        public static void InitializeGame(PictureBox pb, int regular, int monster, int repelent)
         {
             CreateBalls(pb, 0, regular);
             CreateBalls(pb, 1, monster);
             CreateBalls(pb, 2, repelent);
         }
 
-        private void CreateBalls(PictureBox pb, int type, int n)
+        private static void CreateBalls(PictureBox pb, int type, int n)
         {
             for (int i = 0; i < n; i++)
             {
@@ -28,12 +29,46 @@ namespace BigBallGame
             }
         }
 
-        public void MoveAllBalls(PictureBox pb)
+        private static void MoveAllBalls(PictureBox pb)
         {
             foreach(Ball b in AllBalls)
             {
                 b.MoveBall(pb);
             }
+        }
+
+        public static void Turn(PictureBox pb)
+        {
+            MoveAllBalls(pb);
+            CheckCollision();
+        }
+
+        private static void CheckCollision()
+        {
+            for (int i = 0; i < AllBalls.Count; i++)
+            {
+                for (int j = i; j < AllBalls.Count; j++)
+                {
+                    if (IsCollision(AllBalls[i], AllBalls[j]))
+                        CollisionLogic(AllBalls[i], AllBalls[j]);
+                }
+            }
+        }
+
+        private static bool IsCollision(Ball a, Ball b)
+        {
+            if (Distance(a.Position, b.Position) <= a.Radius + b.Radius) return true;
+            else return false;
+        }
+
+        private static double Distance(Point a, Point b)
+        {
+            return Math.Sqrt((a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y));
+        }
+
+        private static void CollisionLogic(Ball a, Ball b)
+        {
+
         }
     }
 }
